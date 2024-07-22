a4a89ea3d1fbeacea9886786b913c35f25eda409
Devil <devilandcode@Devils-MacBook-Pro.local>
2024-07-22 18:41:28 +0300

    add PriceCast

--- src/Domain/Cart/Models/CartItem.php
+++ src/Domain/Cart/Models/CartItem.php
@@ -3,6 +3,7 @@
 namespace Domain\Cart\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class CartItem extends Model
 {
---
+++ src/Support/Casts/PriceCast.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Support\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+class PriceCast implements CastsAttributes
+{
+    public function get($model, string $key, $value, array $attributes): Price
+    {
+        return Price::make($value);
+    }
+
+    public function set($model, string $key, $value, array $attributes): int
+    {
+        if (!$value instanceof Price) {
+            $value = Price::make($value);
+        }
+        return $value->raw();
+    }
+}
