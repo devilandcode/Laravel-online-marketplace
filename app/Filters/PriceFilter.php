b80881c9fbbbae4c1b689a577c8f9a9ebb464360
Devil <devilandcode@Devils-MacBook-Pro.local>
2024-07-22 19:06:41 +0300

    add PriceFilter

--- app/Filters/PriceFilter.php
+++ app/Filters/PriceFilter.php
@@ -4,42 +4,42 @@
 namespace App\Filters;
 
 
-use Domain\Catalog\Models\Brand;
 use Domain\Catalog\Filters\AbstractFilter;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 
-class BrandFilter extends AbstractFilter
+class PriceFilter extends AbstractFilter
 {
 
     public function title(): string
     {
-        return 'Бренды';
+        return 'Цена';
     }
 
     public function key(): string
     {
-        return 'brands';
+        return 'price';
     }
 
     public function apply(Builder $query): Builder
     {
         return $query->when($this->requestValue(), function (Builder $q) {
-            $q->whereIn('brand_id', $this->requestValue());
+            $q->whereBetween('price', [
+                $this->requestValue('from', 0) * 100,
+                $this->requestValue('to', 100000) * 100
+            ]);
         });
     }
 
     public function values(): array
     {
-        return Brand::query()
-            ->select(['id', 'title'])
-            ->has('products') // select records than have products
-            ->get()
-            ->pluck('title', 'id')
-            ->toArray();
+        return [
+            'from' => 0,
+            'to' => 100000
+        ];
     }
 
     public function view(): string
     {
-        return 'catalog.filters.brands';
+        return 'catalog.filters.price';
     }
 }
