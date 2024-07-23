d6d50b4808c435db56cfadfbb1b4bc77802c3e73
Devil <devilandcode@Devils-MacBook-Pro.local>
2024-07-23 14:29:18 +0300

    modify DomainServiceProvider

--- app/Providers/DomainServiceProvider.php
+++ app/Providers/DomainServiceProvider.php
@@ -2,15 +2,35 @@
 
 namespace App\Providers;
 
+use Domain\Auth\Providers\AuthServiceProvider;
+use Domain\Cart\Providers\CartServiceProvider;
+use Domain\Catalog\Providers\CatalogServiceProvider;
+use Domain\Order\Providers\OrderServiceProvider;
+use Domain\Product\Providers\ProductServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 class DomainServiceProvider extends ServiceProvider
 {
+
     public function register(): void
     {
-       $this->app->register(
-           \Domain\Auth\Providers\AuthServiceProvider::class,
-       );
-    }
+        $this->app->register(
+            OrderServiceProvider::class
+        );
 
+        $this->app->register(
+            CartServiceProvider::class
+        );
+
+        $this->app->register(
+            ProductServiceProvider::class
+        );
+
+        $this->app->register(
+            AuthServiceProvider::class
+        );
+        $this->app->register(
+            CatalogServiceProvider::class
+        );
+    }
 }
